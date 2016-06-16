fa204418452ac622cdb933008dc2e89181e69cde
José Ernesto Ocampo Cicero <ernesto@sistrategia.com>
2016-06-16 18:25:07 -0500

    Initial upload.

---
+++ ASPNETIdentityGettingStarted/Startup.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ASPNETIdentityGettingStarted.Startup))]
+namespace ASPNETIdentityGettingStarted
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # ASPNETIdentityGettingStarted
-ASP.NET Identity (2.2.1) Getting Started Sample Applicaiton 01
+This ASP.NET Identity Getting Started Sample Application shows the minimal code of the Visual Studio 2013 templates to Register, Log-in and Log-out an user with the Microsoft ASP.NET Identity version 2.0 to 2.2.1
